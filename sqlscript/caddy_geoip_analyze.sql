e64a7267694f292564813d5701c62f306540dc56
losfair <zhy20000919@hotmail.com>
2021-10-28 02:52:26 +0000

    Update geoip analysis script

--- sqlscript/caddy_geoip_analyze.sql
+++ sqlscript/caddy_geoip_analyze.sql
@@ -1,15 +1,14 @@
 with
   ipt as (
     select ts, host, substring_index(remote_addr, ':', 1) as ip
-    from logs
+    from caddy_log.logs
     where ts > date_sub(current_timestamp(6), interval 1 day)
   ),
   unique_ipt as (
-    select max(ip) as ip, max(ts) as ts, count(*) as cnt,
-            group_concat(distinct host separator ',') as hosts
-      from ipt group by ipt.ip
+    select max(ip) as ip, max(ts) as ts, count(*) as cnt, host
+      from ipt group by ipt.ip, ipt.host
   ),
-  ipgeo as (select ts, ip, cnt, hosts, geoip.query_city_geoname_id_by_ipv4(ip) as geoid from unique_ipt)
-  select /*+ NO_MERGE(ipgeo) */ ipgeo.ip, ipgeo.ts, ipgeo.cnt, ipgeo.hosts, iploc.country_name, iploc.city_name from ipgeo
+  ipgeo as (select ts, ip, cnt, host, geoip.query_city_geoname_id_by_ipv4(ip) as geoid from unique_ipt)
+  select /*+ NO_MERGE(ipgeo) */ ipgeo.ip, ipgeo.ts, ipgeo.cnt, ipgeo.host, iploc.country_name, iploc.city_name from ipgeo
     left join geoip.geoip_city_locations as iploc on iploc.geoname_id = ipgeo.geoid and iploc.locale_code = 'zh-CN'
     order by ipgeo.ts asc;
