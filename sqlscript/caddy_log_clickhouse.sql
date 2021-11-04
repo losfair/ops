acb1c48ea46e513c78dba188b5b4d8a91aff515e
losfair <zhy20000919@hotmail.com>
2021-11-04 10:00:43 +0000

    Update caddy_log_clickhouse

--- sqlscript/caddy_log_clickhouse.sql
+++ sqlscript/caddy_log_clickhouse.sql
@@ -1,20 +1,19 @@
-CREATE TABLE `logs` (
-  `file_id` String,
-  `line_no` Int32,
-  `ts` DateTime,
-  `user_id` String,
-  `duration` Float64,
-  `size` UInt64,
-  `status_code` UInt16,
-  `resp_headers` String,
-  `remote_addr` String,
-  `proto` String,
-  `method` String,
-  `host` String,
-  `uri` String,
-  `req_headers` String,
-  `analytics_blueboat_request_id` String
+create table `logstream_queue` (
+  `raw` String
 )
-ENGINE = ReplacingMergeTree
-PARTITION BY toYYYYMMDD(ts)
-ORDER BY (file_id, line_no);
+engine = Kafka()
+settings
+  kafka_broker_list = '100.99.11.35:9093',
+  kafka_topic_list = 'net.univalent.caddy-log.app,net.univalent.caddy-log.lab',
+  kafka_group_name = 'net.univalent.lab.clickhouse',
+  kafka_format = 'LineAsString';
+
+create table `logstream_rawdata`  (
+  `ts` DateTime,
+  `raw` String
+)
+engine = MergeTree()
+order by `ts`;
+
+create materialized view `logstream_rawdata_consumer` to `logstream_rawdata` as
+  select fromUnixTimestamp64Milli(toInt64(JSONExtractFloat(`raw`, 'ts') * 1000)) as ts, `raw` from `logstream_queue`;
