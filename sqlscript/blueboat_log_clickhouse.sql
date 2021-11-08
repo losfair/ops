031a2121193b391cd1990582b42f42aaeb0c9cb1
losfair <zhy20000919@hotmail.com>
2021-11-08 09:36:14 +0000

    Add lab blueboat views

--- sqlscript/blueboat_log_clickhouse.sql
+++ sqlscript/blueboat_log_clickhouse.sql
@@ -10,7 +10,29 @@ settings
   kafka_group_name = 'net.univalent.lab.clickhouse',
   kafka_format = 'RawBLOB';
 
+create table `logstream_queue_lab` (
+  `raw` String
+)
+engine = Kafka()
+settings
+  kafka_broker_list = 'kafkasvc.default.svc.cluster.local:9092',
+  kafka_topic_list = 'net.univalent.blueboat-log.default',
+  kafka_group_name = 'net.univalent.lab.clickhouse',
+  kafka_format = 'RawBLOB';
+
 create table `logstream_data`  (
+  `ts` DateTime64(3),
+  `apppath` String,
+  `appversion` String,
+  `request_id` String,
+  `message` String,
+  `logseq` UInt32
+)
+engine = MergeTree()
+order by `ts`
+partition by toYYYYMMDD(ts);
+
+create table `logstream_data_lab`  (
   `ts` DateTime64(3),
   `apppath` String,
   `appversion` String,
@@ -38,3 +60,20 @@ select
   JSONExtract(raw, 'message', 'String') as `message`,
   JSONExtract(raw, 'logseq', 'UInt32') as logseq
   from logstream_queue;
+
+create materialized view `logstream_data_lab_consumer` to `logstream_data_lab` as
+select
+  JSONExtract(raw, 'time', 'Array(Int64)') as raw_time,
+  fromUnixTimestamp64Milli(cast(
+    dateAdd(second, raw_time[3],
+      dateAdd(day, raw_time[2],
+        toDate(concat(toString(raw_time[1]), '-01-01'))
+      )
+    ) as Int64
+  ) * 1000 + intDiv(raw_time[4], 1000000)) as ts,
+  JSONExtract(raw, 'app', 'path', 'String') as apppath,
+  JSONExtract(raw, 'app', 'version', 'String') as appversion,
+  JSONExtract(raw, 'request_id', 'String') as request_id,
+  JSONExtract(raw, 'message', 'String') as `message`,
+  JSONExtract(raw, 'logseq', 'UInt32') as logseq
+  from logstream_queue_lab;
