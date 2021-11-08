de44dff123d1fe40406e347a56eefd1cb15a9fa8
losfair <zhy20000919@hotmail.com>
2021-11-08 05:12:25 +0000

    logstream_requestinfo

--- sqlscript/caddy_log_clickhouse.sql
+++ sqlscript/caddy_log_clickhouse.sql
@@ -65,3 +65,20 @@ create materialized view logstream_requests_consumer to logstream_requests as
     ) as resp_headers
     from logstream_queue \
     where JSONExtractString(raw, 'msg') = 'handled request';
+
+create view logstream_requestinfo as
+  select
+    ts, duration, user_id, status, size,
+    splitByChar(':', remote_addr)[1] as ip,
+    proto, method, host, uri,
+    tupleElement(arrayFirst(x -> tupleElement(x, 1) = 'User-Agent', req_headers), 2)[1] as ua,
+    tupleElement(arrayFirst(x -> tupleElement(x, 1) = 'Referer', req_headers), 2)[1] as referrer,
+    tupleElement(arrayFirst(x -> tupleElement(x, 1) = 'Origin', req_headers), 2)[1] as origin,
+    tupleElement(arrayFirst(x -> tupleElement(x, 1) = 'X-Blueboat-Request-Id', resp_headers), 2)[1] as blueboat_reqid,
+    loc.country_name, loc.subdivision_1_name, loc.subdivision_2_name, loc.city_name
+  from logstream_requests as req
+  left any join geoip.city_locations as loc on loc.geoname_id = dictGetUInt32(
+    'geoip_city_blocks_ipv4',
+    'geoname_id',
+    tuple(IPv4StringToNum(ip))
+  );
