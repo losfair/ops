a8e45cba0afc096c75becbe56a9ae65c2ff9bde9
losfair <zhy20000919@hotmail.com>
2022-01-11 17:07:57 +0800

    Cleanup old config

--- sqlscript/blueboat_syslog_clickhouse.sql
+++ sqlscript/blueboat_syslog_clickhouse.sql
@@ -1,15 +1,5 @@
 use blueboat_syslog;
 
-create table `inbound` (
-  `raw` String
-)
-engine = Kafka()
-settings
-  kafka_broker_list = 'kafkasvc.core.svc.cluster.local:9092',
-  kafka_topic_list = 'net.univalent.usw-miniprod.blueboat-log.sys',
-  kafka_group_name = 'net.univalent.usw-miniprod.clickhouse',
-  kafka_format = 'RawBLOB';
-
 create table `inbound_2` (
   `raw` String
 )
@@ -37,22 +27,6 @@ create table `logs`  (
 engine = MergeTree()
 order by `ts`
 partition by toYYYYMM(ts);
-
-create materialized view `logs_consumer` to `logs` as
-select
-  JSONExtract(raw, 'host', 'String') as host,
-  JSONExtract(raw, 'pid', 'Int32') as pid,
-  JSONExtract(raw, 'tid', 'String') as tid,
-  fromUnixTimestamp64Micro(cast(JSONExtract(raw, 'ts', 'Float64') * 1000000 as Int64)) as ts,
-  JSONExtract(raw, 'name', 'String') as name,
-  JSONExtract(raw, 'target', 'String') as target,
-  JSONExtract(raw, 'level', 'String') as level,
-  JSONExtract(raw, 'module_path', 'String') as module_path,
-  JSONExtract(raw, 'file', 'String') as file,
-  JSONExtract(raw, 'line', 'Int32') as line,
-  cast(JSONExtractKeysAndValues(raw, 'fields', 'String'), 'Map(String, String)') as fields,
-  JSONExtract(raw, 'span_id', 'String') as span_id
-  from inbound;
 
 create materialized view `logs_consumer_2` to `logs` as
 select
